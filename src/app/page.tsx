ae426258842e37f7602606316eecdc9d4cae6902
raloboq <rene.lobo.q@gmail.com>
2024-04-06 19:26:28 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from 'next/link'
 export default function Page() {
-    return <h1>Hello, TKPS</h1>
+    return (
+    <>
+    <h1>Hello, TKPS</h1>
+    <Link href="/think">Think</Link>
+    </>)
+    
   }
