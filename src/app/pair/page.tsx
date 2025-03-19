6e6bccddc19937260669481a29b2ad55bbaca887
raloboq <rene.lobo.q@gmail.com>
2025-03-19 10:52:04 -0500

    cambio collaborativeeditor

--- src/app/pair/page.tsx
+++ src/app/pair/page.tsx
@@ -1,4 +1,4 @@
-import { Room } from "@/app/pair/Room";
+/*import { Room } from "@/app/pair/Room";
 
 import { CollaborativeEditor } from "@/components/CollaborativeEditor";
 
@@ -8,6 +8,30 @@ export default function Home() {
     <main>
       <Room>
         <CollaborativeEditor />
+      </Room>
+    </main>
+  );
+}*/
+'use client';
+
+import { Room } from "@/app/pair/Room";
+import { CollaborativeEditor } from "@/components/CollaborativeEditor"; // Asegúrate de que la ruta sea correcta
+import { useSearchParams } from 'next/navigation';
+
+export default function Home() {
+  const searchParams = useSearchParams();
+  const documentId = searchParams.get('roomId') || 'default-room-id';
+  const userName = searchParams.get('alias') || 'Usuario Anónimo';
+  const roomName = searchParams.get('roomName') || 'Sala Default';
+
+  return (
+    <main>
+      <Room>
+        <CollaborativeEditor 
+          documentId={documentId} 
+          userName={userName} 
+          roomName={roomName} 
+        />
       </Room>
     </main>
   );
