eda904a3b08444bcb7d2cab3f57f7ee94a9a6e96
raloboq <rene.lobo.q@gmail.com>
2025-04-29 22:18:09 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/ktps',
-    assetPrefix: '/ktps/',
+    //basePath: '/ktps',
+    //assetPrefix: '/ktps/',
     reactStrictMode: true,
     images: {
       domains: ['i.imgur.com','virtual.konradlorenz.edu.co'],
       unoptimized: true  // Esto puede ayudar con problemas de imágenes en producción
 
     },
-    experimental: {
-      // Si estás usando Next.js 13 o superior con App Router
-      appDir: true
-    }
+    
   }
 
 module.exports = nextConfig
