233ffde0cba3d2381d3b3ef4e2d257d4b58edf09
raloboq <rene.lobo.q@gmail.com>
2025-04-29 20:45:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/ktps',
-    assetPrefix: '/ktps/',
+    //basePath: '/ktps',
+    //assetPrefix: '/ktps/',
     reactStrictMode: true,
     images: {
       domains: ['i.imgur.com','virtual.konradlorenz.edu.co'],
