e6318f5ecefbb80e2d88c1f952bfabb41c339d73
raloboq <rene.lobo.q@gmail.com>
2025-04-29 22:11:00 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,13 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
       domains: ['i.imgur.com','virtual.konradlorenz.edu.co'],
-      
+      unoptimized: true  // Esto puede ayudar con problemas de imágenes en producción
+
     },
+    experimental: {
+      // Si estás usando Next.js 13 o superior con App Router
+      appDir: true
+    }
   }
 
 module.exports = nextConfig
